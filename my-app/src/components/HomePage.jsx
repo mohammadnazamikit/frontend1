07de4f5272aeca7aa106464389c1ee3384bc4b5a
mohammadnazamikit <108289537+mohammadnazamikit@users.noreply.github.com>
2022-12-08 18:26:51 +0100

    leaflet fixed

--- my-app/src/components/HomePage.jsx
+++ my-app/src/components/HomePage.jsx
@@ -5,13 +5,9 @@ import Navbar2 from "./Navbar2";
 const HomePage = (props) => {
   return (
     <>
-      <div>
-        <Navbar1></Navbar1>
-        <Navbar2></Navbar2>
-      </div>
-      <div>
-        <Map />
-      </div>
+      <Navbar1></Navbar1>
+      <Navbar2></Navbar2>
+      <Map />
     </>
   );
 };
--- my-app/src/components/Map.jsx
+++ my-app/src/components/Map.jsx
@@ -1,15 +1,16 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const Map = (props) => {
   return (
     <MapContainer
+      style={{ height: "60vh", width: "100vh", margin: "100px" }}
       center={[51.505, -0.09]}
       zoom={13}
-      scrollWheelZoom={false}
-      style={{ height: "30px", width: "30px" }}
+      scrollWheelZoom={true}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap </a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={[51.505, -0.09]}>
